1f082457e593ca5c668731e3bdd7b7f2230ab97d
MarekDudek <marek.dudek@gmail.com>
2023-04-01 00:52:07 +0200

    fizz buzz example refactor

--- src/test/java/md/jqwickdemo/FizzBuzzTests.java
+++ src/test/java/md/jqwickdemo/FizzBuzzTests.java
@@ -3,30 +3,40 @@ package md.jqwickdemo;
 import net.jqwik.api.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
 
 public class FizzBuzzTests
 {
     @Property
-    boolean every_third_element_starts_with_Fizz(@ForAll("divisibleBy3") int i) {
+    boolean every_third_element_starts_with_Fizz(@ForAll("divisibleBy3") int i)
+    {
         return fizzBuzz().get(i - 1).startsWith("Fizz");
     }
 
     @Provide
-    Arbitrary<Integer> divisibleBy3() {
-        return Arbitraries.integers().between(1, 100).filter(i -> i % 3 == 0);
+    @SuppressWarnings("unused")
+    private Arbitrary<Integer> divisibleBy3()
+    {
+        return Arbitraries.
+                integers().
+                between(1, 100).
+                filter(i -> i % 3 == 0);
     }
 
-    private List<String> fizzBuzz() {
-        return IntStream.range(1, 100).mapToObj((int i) -> {
-            boolean divBy3 = i % 3 == 0;
-            boolean divBy5 = i % 5 == 0;
-
-            return divBy3 && divBy5 ? "FizzBuzz"
-                    : divBy3 ? "Fizz"
-                    : divBy5 ? "Buzz"
-                    : String.valueOf(i);
-        }).collect(Collectors.toList());
+    private List<String> fizzBuzz()
+    {
+        return IntStream.
+                range(1, 100).
+                mapToObj(i -> {
+                            boolean by3 = i % 3 == 0;
+                            boolean by5 = i % 5 == 0;
+                            return by3 && by5 ? "FizzBuzz"
+                                    : by3 ? "Fizz"
+                                    : by5 ? "Buzz"
+                                    : String.valueOf(i);
+                        }
+                ).collect(toList());
     }
 }
